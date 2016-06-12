e23f4b47aa39efd622d5d59730c190dee6821714
Coleman McFarland <coleman.mcfarland@deciphertechstudios.com>
2016-06-12 14:50:17 -0400

    Read in config values. Added config template

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
 
 func main() {
 
 	// read config
+	viper.SetConfigName("conf")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config: %v\n", err)
+	}
+
+	// Get API token and refresh token
+	tkn := viper.GetString("api_token")
+	rfsh := viper.GetString("refresh_token")
 
 	// start server
 	fmt.Println("Hello")
+
+}
+
+func DoRequest(token string) error {
 
 }
